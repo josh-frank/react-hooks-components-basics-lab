04f4e3ca1518a61f28fe10a7bdd7d48e5fecf213
Josh Frank <eurosewusa@gmail.com>
2021-02-21 20:41:15 -0500

    commit tree 88d874a39f0ff14448d98344548e13bab53d6323

--- src/components/App.js
+++ src/components/App.js
@@ -16,12 +16,20 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="About">
+      <h2>All about me</h2>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      <Home />
+      {/* <About /> */}
     </div>
   );
 }
